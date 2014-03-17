594890a2f31a97575d8c4ecf544ae387d6690873
seanzawi <seanzawicki@gmail.com>
2014-03-16 23:23:12 -0700

    updates from Kit

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+var assert			= require('assert'),
+	events			= require('events').EventEmitter,
+	fs				= require('fs'),
+	git				= require('gift'),
+	initialize		= require('./lib/initialize'),
+	path			= require('path'),
+	restify			= require('restify'),
+	routes			= require('./lib/routes.js'),
+	serverControl	= require('./lib/start_server'),
+	shell			= require('shelljs'),
+	url				= require('url')
+	;
+
 module.exports = function(options)
 {
-	var assert			= require('assert'),
-		events			= require('events').EventEmitter,
-		fs				= require('fs'),
-		git				= require('gift'),
-		initialize		= require('./lib/initialize'),
-		path			= require('path'),
-		restify			= require('restify'),
-		routes			= require('./lib/routes.js'),
-		serverControl	= require('./lib/start_server'),
-		shell			= require('shelljs'),
-		util			= require('util'),
-		opts			= 
+	if (!options)
+		options = {};
+
+	var opts	=
 		{
 			port:		options.port    || 9001,
-			verbose:	(typeof options.verbose === 'undefined') ? true : options.verbose
+			verbose:	(options.verbose == null) ? true : options.verbose
 		};
 
 	if(Array.isArray(options.repo))
@@ -27,18 +32,15 @@ module.exports = function(options)
 	var repo_names = {};
 	opts.repo.forEach(function(repository, index)
 	{
-		if(repository.start === undefined)
-			repository.start = 'node index.js';			
-		if(repository.install === undefined)
+		if(repository.start == null)
+			repository.start = 'node index.js';
+		if(repository.install == null)
 			repository.install = 'npm i';
 
-		if(repository.url === undefined)
-			throw('you must provide a valid git url');
-		var repo = repository.url.split('/');
-		repo_names[repo[repo.length-1].split('.git')[0]] = repository;
+		assert(repository.url != null, 'you must provide a valid git url');
+		repo_names[path.basename(url.parse(repository.url).pathname, '.git')] = repository;
 	});
 	opts.repo = repo_names;
-	util.inherits(server, events);
 	initialize(opts.repo, postServer);
 
 	function serverPull(repository, next)
@@ -126,16 +128,24 @@ module.exports = function(options)
 	server.get('/results/:name', function(req, res, next) {routes.getList(req, res, next, servers);});
 	server.get('/list', getList);
 	server.get('/ping', routes.ping);
-	
+
 	server.listen(opts.port, function() {
 		server.emit('log', 'hook-line-sinker listening at http://localhost:'+opts.port);
 	});
 
-	function postServer(item)
+	function postServer(err, items)
 	{
+		if (err)
+		{
+			server.emit('log', 'Server failed to initialize.');
+			return server.emit('error', err);
+		}
 		server.emit('log', 'Server has been initialized, moving on to pulling code down.');
-		server.emit('commit', item);
+		items.forEach(function(item)
+		{
+			server.emit('commit', item);
+		});
 	}
-	
+
 	return server;
 };
